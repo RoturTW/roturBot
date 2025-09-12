a169b44100cf6574dabb8cebcb88d58a473a05be
Sophie <d.a.kimber@icloud.com>
2025-09-12 02:12:00 +0100

    removed some keys that were unnessecary to block. tho a couple i removed are system managed

--- commands/roturacc.py
+++ commands/roturacc.py
@@ -5,7 +5,7 @@ from ..helpers import rotur
 MISTIUM_ID = "603952506330021898"
 
 async def query(spl, channel, user, dir):
-    restrictedKeys = ["username", "last_login", "max_size", "key", "created", "password", "system", "id", "sys.currency"]
+    restrictedKeys = ["username", "max_size", "created", "id", "sys.currency"]
 
     with open(os.path.join(dir, '..', 'systems.json'), 'r') as f:
         systems = json.load(f)
