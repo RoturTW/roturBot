7a2ff9871d5e7307d7147b0f84867cea72190de3
Sophie <d.a.kimber@icloud.com>
2025-09-18 07:03:07 +0100

    marriage on bios

--- helpers/rotur.py
+++ helpers/rotur.py
@@ -4,12 +4,16 @@ import requests
 def bio_from_obj(obj):
     tier = str(obj.get("subscription", "Free"))
 
-    return (
+    string = (
         f'{tier}\n'
         f'Credits: {obj.get("currency", "0")}\n'
         f'Account: #{obj.get("index", "unknown")}\n\n'
         f'{obj.get("bio", "")}\n'
     )
+    if obj.get("married_to"):
+        string += f'💍 Married to {obj["married_to"]}\n'
+    
+    return string
 
 def get_user_by(key, value):
     response = requests.get(f'https://social.rotur.dev/admin/get_user_by?key={key}', json={"value": value}, headers={'Authorization': os.getenv("ADMIN_TOKEN")})
